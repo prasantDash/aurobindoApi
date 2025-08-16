88a211fdba6ddc04f3abdcb5da7f739c65632eb6
prasantdash01012017 <prasantdash01012017@gmail.com>
2025-08-16 18:20:57 +0530

    User action

--- dboperations.js
+++ dboperations.js
@@ -1,8 +1,10 @@
 const mysql = require('mysql');
+const { ncrypt } = require("ncrypt-js");
+const jwt = require('jsonwebtoken');
 const config = require('./dbConfig');
-const { json } = require('body-parser');
 
 const connection = mysql.createConnection(config);
+
 
 async function getProducts() {
     try {
@@ -14,12 +16,116 @@ async function getProducts() {
                 }
                 resolve(results);
             });
-        });        
+        });
     } catch (error) {
         console.log(error);
     }
 }
 
+async function getUsers() {
+    try {
+        return new Promise((resolve, reject) => {
+            connection.query("SELECT * FROM Users", (error, results) => {
+                if (error) {
+                    console.error("Error fetching User:", error);
+                    return reject(error);
+                }
+                resolve(results);
+            });
+        });
+    } catch (error) {
+        console.log(error);
+    }
+
+}
+async function createUsers(data) {
+    const secretKey = "aurobindo";
+    try {
+        return new Promise((resolve, reject) => {
+            var { encrypt } = new ncrypt(secretKey);
+            const { username, email, password_hash } = data;
+            const query = "INSERT INTO Users (username, email, password_hash,active) VALUES (?, ?, ?,?)";
+            const values = [username, email, encrypt(password_hash), "1"];
+            connection.query(query, values, (error, results) => {
+                if (error) {
+                    console.error("Error creating user:", error);
+                    return reject(error);
+                }
+                console.log("User created successfully:", results);
+                resolve(results);
+            });
+        });
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+async function getUserProfile(token) {
+    const secretKey = "aurobindo";
+    try {
+        return new Promise((resolve, reject) => {           
+            const decoded = jwt.verify(token.token, secretKey); 
+
+            const userId = decoded.id;
+            const query = "SELECT * FROM profiles WHERE id = ?";
+
+            connection.query(query, [userId], (error, results) => {
+                if (error) {
+                    console.error("Error fetching user profile:", error);
+                    return reject(error);
+                }
+                resolve(results[0]);
+            });
+        });
+    } catch (error) {
+        console.log(error);
+    }
+
+}
+async function usersLogin(data) {
+    const secretKey = "aurobindo";
+    try {
+        return new Promise((resolve, reject) => {
+
+            var { encrypt, decrypt } = new ncrypt(secretKey);
+            const { username, password_hash } = data;
+            console.log("Username:", username);
+            console.log("Decrypt password:", encrypt(password_hash));
+            const query = "SELECT * FROM users WHERE username=?";
+
+            const values = [username];
+            //console.log("Query:", query, "Values:", values);
+
+            connection.query(query, values, (error, results) => {
+                if (error) {
+                    reject(error);
+                }
+                console.log("User status:", results[0].active);
+                if (results[0].active === 0) {
+                    resolve({ message: 'User is inactive' });
+                }
+                if (decrypt(results[0].password_hash) === password_hash) {
+                    const token = jwt.sign({ id: results[0].id, username: results[0].username }, secretKey, { expiresIn: '1h' });
+                    const response = {
+                        id: results[0].id,
+                        username: results[0].username, 
+                        token: token,
+                        email: results[0].email,
+                    }
+
+                    resolve(response);
+                }
+                resolve({ message: 'Invalid password' });
+            });
+        });
+    } catch (error) {
+        console.log(error);
+    }
+}
 module.exports = {
-    getProducts: getProducts
+    getProducts: getProducts,
+    getUsers: getUsers,
+    createUsers: createUsers,
+    usersLogin: usersLogin,
+    getUserProfile: getUserProfile
 };
--- index.js
+++ index.js
@@ -12,13 +12,58 @@ app.use(cors());
 app.get('/api/products', async (req, res) => {
     try {
         const products = await dboperations.getProducts();
-        if (!products) {
-            return res.status(404).send('No products found');
-        }
         if (!products || products.length === 0) {
             return res.status(404).send('No products found');
-        }   
+        }
         res.json(products);
+    } catch (error) {
+        res.status(500).send('Error fetching products');
+    }
+});
+
+app.get('/api/users', async (req, res) => {
+    try {
+        const users = await dboperations.getUsers();
+        if (!users || users.length === 0) {
+            return res.status(404).send('No User found');
+        }
+        res.json(users);
+    } catch (error) {
+        res.status(500).send('Error fetching products');
+    }
+});
+
+app.post('/api/createUsers', async (req, res) => {
+    try {
+        const users = await dboperations.createUsers(req.body);
+        if (!users || users.length === 0) {
+            return res.status(404).send('No User found');
+        }
+        res.json(users.affectedRows > 0 ? { message: 'User created successfully' } : { message: 'User creation failed' });
+    } catch (error) {
+        res.status(500).send('Error fetching products');
+    }
+});
+
+app.post('/api/user/login', async (req, res) => {
+    try {
+        const users = await dboperations.usersLogin(req.body);
+        if (!users || users.length === 0) {
+            return res.status(404).send('No User found');
+        }
+        res.json(users);
+    } catch (error) {
+        res.status(500).send('Error fetching products');
+    }
+});
+
+app.post('/api/user/profile', async (req, res) => {
+    try {
+        const userProfile = await dboperations.getUserProfile(req.body);
+        if (!userProfile || userProfile.length === 0) {
+            return res.status(404).send('No User found');
+        }
+        res.json(userProfile);
     } catch (error) {
         res.status(500).send('Error fetching products');
     }
