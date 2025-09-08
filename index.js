86abee849c7544373faecc55ea3acad29226c96f
prasantdash01012017 <prasantdash01012017@gmail.com>
2025-09-09 01:52:39 +0530

    Port updated

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const cors = require('cors');
 const dboperations = require('./dboperations');
 
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 8000;
 
 app.use(bodyParser.json());
 app.use(cors());
