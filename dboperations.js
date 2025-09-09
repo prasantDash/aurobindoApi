b2bda0d303aaaa141f17723e1c6bebddba44bd19
prasantdash01012017 <prasantdash01012017@gmail.com>
2025-09-09 23:09:05 +0530

    console added

--- dboperations.js
+++ dboperations.js
@@ -136,11 +136,13 @@ async function getUserAuthStatus(token){
 
 async function saveUnitdata(data){
     const unitData = data;
+    console.log("Received unit data:", unitData);
     const dateAndTime = unitData.timestamp;
     unitData.lat = unitData.geocode.split(",")[0];
     unitData.lng = unitData.geocode.split(",")[1];
     unitData.dataAndTime = dateAndTime;
     const insertQuery = `INSERT INTO unitdata.unitdata(tripId, driverId, deviceId, appVersion, geocode, timestamp, platform, speed, userId, lat, lng, dataAndTime) VALUES ('${unitData.tripId}','${unitData.driverId}','${unitData.deviceId}','${unitData.appVersion}','${unitData.geocode}','${unitData.timestamp}','${unitData.platform}','${unitData.speed}','${unitData.userId}','${unitData.lat}','${unitData.lng}','${unitData.dataAndTime}')`;
+    console.log("Insert Query:", insertQuery);
     connection.query(insertQuery, (error, results) => {
         console.log(results)
         if (error) {
