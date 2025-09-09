0e3930c6f3b7e7d7011c333230a73fe8deba8d3d
prasantDash <35262173+prasantDash@users.noreply.github.com>
2025-09-10 00:13:10 +0530

    Update dboperations.js

--- dboperations.js
+++ dboperations.js
@@ -155,19 +155,19 @@ function formattDate(indate) {
 
 async function saveUnitdata(data) {
   const unitData = data;
-  console.log("Received unit data:", unitData);
+  //console.log("Received unit data:", unitData);
   const dateAndTime = formattDate(unitData.timestamp);
   unitData.lat = unitData.geocode.split(",")[0];
   unitData.lng = unitData.geocode.split(",")[1];
   unitData.dataAndTime = formattDate(dateAndTime);
   const insertQuery = `INSERT INTO unitdata(tripId, driverId, deviceId, appVersion, geocode, timestamp, platform, speed, userId, lat, lng, dataAndTime) VALUES ('${unitData.tripId}','${unitData.driverId}','${unitData.deviceId}','${unitData.appVersion}','${unitData.geocode}','${dateAndTime}','${unitData.platform}','${unitData.speed}','${unitData.userId}','${unitData.lat}','${unitData.lng}','${unitData.dataAndTime}')`;
-  console.log("Insert Query:", insertQuery);
+  //console.log("Insert Query:", insertQuery);
   connectionUnitData.query(insertQuery, (error, results) => {
     if (error) {
-      console.error("Error inserting unit data:", error);
+      //console.error("Error inserting unit data:", error);
       return "Error on inserttion:";
     } else {
-      console.log("Unit data inserted successfully:", results);
+      //console.log("Unit data inserted successfully:", results);
       return { message: "created successfully" };
     }
   });
