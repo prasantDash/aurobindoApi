bfeb40c5be51e4dd3af2023c5583e490cd31553e
prasantDash <35262173+prasantDash@users.noreply.github.com>
2025-09-09 23:37:23 +0530

    Update dbConfig.js

--- dbConfig.js
+++ dbConfig.js
@@ -3,7 +3,7 @@ const config = {
     password: 'root',
     server: 'localhost', // e.g., 'localhost', 'SERVER_IP_ADDRESS'
     port: 3306,
-    database: 'prasant',
+    database: 'prasant_test',
     connectionLimit: 10, // Optional: Set the maximum number of connections in the pool
     options: {
         encrypt: false, // Use true if you're connecting to Azure SQL Database
